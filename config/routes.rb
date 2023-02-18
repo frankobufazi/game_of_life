926260db047c5f6747ad7af5f7ba331153880d91
Franko Bufazi <frankobufazi@MBP-di-Franko.homenet.telecomitalia.it>
2023-02-18 13:15:52 +0100

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'main#home'
+  get '/home', to: 'main#home'
+  get '/login', to: 'main#login'
+  get '/my', to: 'main#dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
---
+++ test/controllers/main_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class MainControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get main_home_url
+    assert_response :success
+  end
+
+  test "should get login" do
+    get main_login_url
+    assert_response :success
+  end
+end
